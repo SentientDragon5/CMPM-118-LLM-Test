c880eea25666171444b4588610da6ccd9e787d6a
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2024-12-11 15:00:07 -0800

    clean up old files

--- consoleChat.js
+++
@@ -1,8 +0,0 @@
-import { chatWithAI } from "./llmChat.js";
-import promptSync from "prompt-sync";
-
-async function consolelog(user, text) {
-  console.log(user, ": ", text);
-}
-const prompt = promptSync();
-chatWithAI(consolelog, prompt);
